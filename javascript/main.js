06b1d39cccda92400859388b8a06f2ae4737d128
Anne Haase <a.haase@willhoeft-it.com>
2017-04-03 22:26:12 +0200

    test request to bahn

--- javascript/main.js
+++ javascript/main.js
@@ -118,6 +118,15 @@ $( document ).ready(function() {
                 $(image).attr('data-id', id);
             }
         }
+
+        $.ajax({
+            url: "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?ld=9646&rt=1&input=%23008010382&boardType=dep&time=actual&productsFilter=11111&start=yes",
+            crossDomain: true,
+            dataType: 'text',
+            success: function(data) {
+                console.log('---data', data);
+            }
+        });
     });
 
 
